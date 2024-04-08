d0f2621d25ef07c9e5a09ebf272fdb30b0e9437a
UbitUmarov <ajlduarte@sapo.pt>
2024-04-08 23:06:48 +0100

    update runprebuild. bat/sh

--- runprebuild.bat
+++ runprebuild.bat
@@ -1,5 +1,6 @@
 @echo OFF
 
+copy bin\System.Drawing.Common.dll.win bin\System.Drawing.Common.dll
 dotnet bin\prebuild.dll /target vs2022 /targetframework net6_0 /excludedir = "obj | bin" /file prebuild.xml
 
     @echo Creating compile.bat
