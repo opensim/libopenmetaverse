f555499111c7b2f884de6e30c1d43085c51a7662
UbitUmarov <ajlduarte@sapo.pt>
2024-04-21 10:51:03 +0100

    fix runprebuild comment

--- runprebuild.bat
+++ runprebuild.bat
@@ -4,9 +4,9 @@ copy bin\System.Drawing.Common.dll.win bin\System.Drawing.Common.dll
 dotnet bin\prebuild.dll /target vs2022 /targetframework net8_0 /excludedir = "obj | bin" /file prebuildCoreLib.xml
 
     @echo Creating compile.bat
-rem To compile in debug mode
+rem To compile in release mode
     @echo dotnet build --configuration Release OpenMetaverse.sln > compile.bat
-rem To compile in release mode comment line (add rem to start) above and uncomment next (remove rem)
+rem To compile in debug mode comment line (add rem to start) above and uncomment next (remove rem)
 rem    @echo %ValueValue% /p:Configuration=Release opensim.sln > compile.bat
 :done
 if exist "bin\addin-db-002" (
